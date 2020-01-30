c605fc5b3ededadf3d5420fc63c1622371683468
Lukas-Poslusny <48207694+Lukas-Poslusny@users.noreply.github.com>
2020-01-30 12:41:40 +0100

    Add files via upload

---
+++ router-project/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component'
+import { AboutComponent } from './about/about.component'
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
